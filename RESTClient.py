d48b97125f246b8d488c51d0b96518c720215af5
Hello <prabhjot0083@gmail.com>
2025-06-11 10:52:38 +0530

    update port

--- RESTClient.py
+++ RESTClient.py
@@ -20,6 +20,6 @@ def startup_event():
 
 def runServer():
     try:
-        uvicorn.run(app, host="0.0.0.0", port=8080)
+        uvicorn.run(app, host="0.0.0.0", port=8081)
     except Exception as ex:
         print(f"Error: {ex}")
