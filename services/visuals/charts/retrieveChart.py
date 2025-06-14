eaca1887f93018014b3f89ea081bdd58e334f991
Hello <prabhjot0083@gmail.com>
2025-06-14 15:34:10 +0530

    reset the charts labels

--- services/visuals/charts/retrieveChart.py
+++ services/visuals/charts/retrieveChart.py
@@ -36,30 +36,42 @@ def retrieveChart(
         else:
             months = [(year, m) for m in months]
 
-        xAxis = [f"{calendar.month_abbr[m]}" for (y, m) in months]
+        first_label = chartData[0]["label"].lower()
+        current_year = datetime.now().year
+        if first_label == "current year" or str(current_year) in first_label:
+            xAxis = [f"{calendar.month_abbr[m]}" for (y, m) in months]
+        else:
+            xAxis = [f"{calendar.month_abbr[m]} {y}" for (y, m) in months]
+
+
+       
         yAxisSeries = []
 
         for metric in chartData:
+            valueFunc = metric["valueFunc"]
+            
             label = metric["label"]
             functionName = metric["funcName"]
 
-            valueData = getValueSymbol(label)
+            valueData = getValueSymbol(valueFunc)
             valueType = valueData["type"]
             valueSymbol = valueData["symbol"]
 
-            func = functionRegistry.get(functionName)
+            func = functionRegistry.get(functionName) 
+
+
             if not func:
                 raise ValueError(f"Function '{functionName}' not found in registry.")
 
             values = []
             for (y, m) in months:
                 try:
-                    actual_year = y - 1 if "Prev Year" in label else y
+                    actual_year = y - 1 if "Last Year" in label else y
                     result = func(actual_year, [m], reportId)
                     data = result.Data
                 except Exception as error:
                     data = 0
-                    print(f"{datetime.now()} Error fetching '{label}' for {calendar.month_name[m]} {y}: {error}")
+                    print(f"{datetime.now()} Error fetchiwng '{label}' for {calendar.month_name[m]} {y}: {error}")
                 values.append(data)
 
             yAxisSeries.append(
@@ -92,81 +104,3 @@ def retrieveChart(
         print(f"{datetime.now()} {message}")
         return Result(Status=0, Message=message)
 
-
-# def retrieveChart(
-#     year: int,
-#     months: list[int],
-#     title: str,
-#     chartData: list[dict],
-#     rigthYaxis: str,
-#     axisChoice: str,
-#     reportType: Optional[str] = None,
-#     reportId: Optional[int] = None,
-# ) -> Result:
-#     try:
-#         print(reportType)
-
-#         if reportType.lower() == "month":
-#             months = range(1,months[0]+1)
-        
-#         xAxis = [f"{calendar.month_abbr[m]} " for m in months]
-
-#         # xAxis = [f"{calendar.month_abbr[m]} {year}" for m in staticMonths]
-#         yAxisSeries = []
-
-#         for metric in chartData:
-#             label = metric["label"]
-#             functionName = metric["funcName"]
-
-#             valueData = getValueSymbol(label)
-
-#             valueType = valueData["type"]
-#             valueSymbol = valueData["symbol"]
-
-#             # Get the function from the registry
-#             func = functionRegistry.get(functionName)
-#             if not func:
-#                 raise ValueError(f"Function '{functionName}' not found in registry.")
-
-#             values = []
-
-#             localYear = year - 1 if "Prev Year" in label else year
-
-#             for month in months:
-#                 try:
-#                     result = func(localYear, [month], reportId)
-#                     data = result.Data
-#                 except Exception as error:
-#                     data = 0  # fallback if error
-#                     print(
-#                         f"{datetime.now()} Error fetching '{label}' for month {month}: {error}"
-#                     )
-#                 values.append(data)
-
-#             yAxisSeries.append(
-#                 YAxisSeriesModel(
-#                     Title=label,
-#                     Values=values,
-#                     Type=metric["type"],
-#                     UnitType=valueType,
-#                     Symbol=valueSymbol,
-#                     AreaFill=metric["AreaFill"],
-#                 )
-#             )
-
-#         chartData = ChartDataModel(
-#             Title=title,
-#             Xaxis=xAxis,
-#             YaxisSeries=yAxisSeries,
-#             IndexAxis=axisChoice,
-#             RightYaxis=rigthYaxis,
-#         )
-
-#         return Result(
-#             Data=chartData, Status=1, Message=f"{title} chart generated successfully"
-#         )
-
-#     except Exception as error:
-#         message = f"Error generating chart: {error}"
-#         print(f"{datetime.now()} {message}")
-#         return Result(Status=0, Message=message)
